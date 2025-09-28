6f7ae70956cc6d2834748d0a0cbde50aac4cf108
Swetha <swethageeth33@gmail.com>
2025-09-28 14:06:08 +0530

    changes added

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function footer() {
+    return   <footer className="py-1 bg-dark">
+      <p className="text-center text-white mt-1 ">
+        JVLcart - 2023-2024, All Rights Reserved
+      </p>
+    </footer>
+}
